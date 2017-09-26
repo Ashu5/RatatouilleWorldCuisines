34693a7426f5bd4edbe4ae311a383621165895c9
Ashutosh Kumar Dwivedi <www.ashu5@gmail.com>
2017-09-26 16:53:37 +0530

    Update ImageLibs.java

--- ImageLibs.java
+++ ImageLibs.java
@@ -1,1 +1,15 @@
 
+public class ImageLibs extends AppCompatActivity
+{
+
+public void getImages()
+{
+Button button=(Button)rootView.getElementById(R.imgBtn);
+button.setOnClickListener(View.OnButtonListener)
+{
+
+}
+}
+
+
+}
